2b7953fbe86d6807572229f9b78627cacefd2b0d
Koalakai <kaishinpk@gmail.com>
2018-11-07 22:08:01 -0800

    Add a popover to EventPolling

--- scribble/src/Component/EventPolling.js
+++ scribble/src/Component/EventPolling.js
@@ -6,28 +6,93 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
 import Hidden from '@material-ui/core/Hidden';
-
+import Popover from '@material-ui/core/Popover';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
 const styles = theme => ({
+
 });
 
-function GridTest(props){
     var numbers=[['time1','people: 5','Kaishin, Juhsep, Kaori, Shark, you'],
         ['time2','people: 3','Hello, julie, animals'],
         ['time3','people: 7','kumi, stop, a, talkin, you, agree, Thank you']
     ];
-    const listItems = numbers.map((number)=>
+
+class SimplePopover extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state={
+            value:null,
+        };
+    }
+
+  state = {
+    anchorEl: null,
+  };
+
+  handleClick = event => {
+    this.setState({
+      anchorEl: event.currentTarget,
+    });
+  };
+
+  handleClose = () => {
+    this.setState({
+      anchorEl: null,
+    });
+  };
+
+    render(){
+        const { classes } = this.props;
+    const { anchorEl } = this.state;
+    const open = Boolean(anchorEl);
+
+        return(
         <Grid container spacing = {0}>
-            <Grid item xs={4}><Typography variant="h6">{number[0]}</Typography></Grid>
+            <Grid item xs={4}><Typography variant="h6">{this.props.number[0]}</Typography></Grid>
             <Grid item xs={7}>
-        <Typography variant="h6">{number[1]}</Typography>
-        <Hidden xsDown><Typography paragraph>{number[2]}</Typography></Hidden>
+        <Typography variant="h6">{this.props.number[1]}</Typography>
+        <Hidden xsDown><Typography paragraph>{this.props.number[2]}</Typography></Hidden>
             </Grid>
             <Grid item xs={1}>
-            <IconButton>
+            <IconButton
+        aria-owns={open ? 'simple-popper' : undefined}
+          aria-haspopup="true"
+          variant="contained"
+          onClick={this.handleClick}
+        >
               <MoreVertIcon />
             </IconButton>
+        <Popover
+          id="simple-popper"
+          open={open}
+          anchorEl={anchorEl}
+          onClose={this.handleClose}
+          anchorOrigin={{
+            vertical: 'center',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'center',
+            horizontal: 'right',
+          }}
+        >
+            <List>
+          <ListItem button>Mark as Available</ListItem>
+          <ListItem button>Mark as Unavailable</ListItem>
+            </List>
+        </Popover>
             </Grid>
         </Grid>
+
+        );
+    }
+}
+
+function GridTest(props){
+    const listItems = numbers.map((number)=>
+        <SimplePopover number={number} />
     );
     return(
         <div>
