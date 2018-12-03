0c6a30c370b1cef930ef0cfc29b62a8b07ca5add
seandoerr <sean.doerr@yahoo.com>
2018-12-03 14:24:40 -0800

    Added background to title page to make it look nicer

--- scribble/src/Home.js
+++ scribble/src/Home.js
@@ -2,11 +2,25 @@ import React, {Component} from 'react';
 import ButtonAppBar from './Component/ButtonAppBar';
 import ScribbleHeader from './Component/ScribbleHeader';
 import Login from './Component/Login';
+import { withStyles } from '@material-ui/core/styles';
+import background from './static/meeting.jpg';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    backgroundImage: `url(${background})`,
+    backgroundPosition: 'center',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+  }
+});
 
 class Home extends Component{
   render(){
+
     return(
-      <div>
+      <div className={this.props.classes.root} >
       <ButtonAppBar></ButtonAppBar>
       <ScribbleHeader></ScribbleHeader>
       <Login/>
@@ -14,4 +28,5 @@ class Home extends Component{
     );
   }
 }
-export default Home;
+
+export default withStyles(styles)(Home);
